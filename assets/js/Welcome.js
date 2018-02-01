23dd31b77185e1cad8a5f658ee4c757ff31d0524
alexbeno <alexbenoliel@gmail.com>
2018-02-01 14:47:29 +0100

    🚀 deploys assets folder

---
+++ assets/js/Welcome.js
@@ -0,0 +1,16 @@
+class Welcome
+{
+    /**
+     * Constructor
+     */
+    constructor( options )
+    {
+
+    }
+
+    init() {
+        console.log( '%c made with ღ by Equipe 09' , 'color: #484AE0' )
+    }
+}
+
+export default Welcome
---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+import Welcome from './Welcome.js'
+
+function init() {
+
+  /**
+   * Welcome.js
+   * Welcome function
+   */
+
+  let welcome = new Welcome()
+  welcome.init();
+}
+
+window.onload = init;
